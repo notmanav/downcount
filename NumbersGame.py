411e57227e8ded0cd54477c00b517a0ae45467bc
notmanav <notmanav@github.com>
2018-03-22 14:15:11 -0700

    Stable, released

--- NumbersGame.py
+++ NumbersGame.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 from __future__ import division
 import random
+import re
 from os.path import sys
 from time import sleep
 from math import floor
@@ -10,6 +11,7 @@ class Problem:
         self.numbers=list()
         self.bigpool=[25,50,75,100]
         self.target=0
+        self.winners=set()
     
     def generateProblem(self):
         random.shuffle(self.bigpool)      
@@ -38,64 +40,58 @@ class Problem:
 class Solver:
     
     def solveProblem(self,p):
-        self.recurse(str(p.numbers[0]),p.numbers, p.target)
+        self.recurse("",0,p.numbers, p.target,p.winners,False)
+        print "And the "+str(len(p.winners)) +" winners are..."
+        #sort by number of numbers and then the length of overall string
+        p.winners=sorted(p.winners,key=lambda winner: (len(re.split("[\(\)\\+\-/]+",winner)),len(winner)))
+        for winner in p.winners:
+            print winner + (" = "+str(p.target)) if(eval(winner) ==p.target) else ""
   
     # still has a bug that the first letter on the board is the first letter in solution. maybe adding a no-op value(0/1) as the first item in the list will fix it
-    def recurse(self,stringsofar,numberslist,target):
-        for i in range(1,len(numberslist)):
+    def recurse(self,stringsofar,ressofar,numberslist,target,winners,verbose):
+        for i in range(0,len(numberslist)):
             #Add with previous result
-            res1=numberslist[0]+numberslist[i]
-            stringnow=stringsofar+"+"+str(numberslist[i])
-            if(res1!=target):
-                newlist=list(numberslist)
-                newlist.pop(i)
-                newlist.pop(0)
-                newlist.insert(0, res1)
-                self.recurse(stringnow,newlist,target)
+            if(ressofar==0):
+                resnow=numberslist[i]
+                stringnow=str(numberslist[i])
+                self.validate(stringnow, resnow, numberslist, target, winners,verbose, i)
             else:
+                resnow=ressofar+numberslist[i]
+                stringnow=stringsofar+"+"+str(numberslist[i])               
+                self.validate(stringnow, resnow, numberslist, target, winners,verbose, i)
+                #multiply with previous string
+                resnow=ressofar*numberslist[i]
+                stringnow="("+stringsofar+")*"+str(numberslist[i])
+                self.validate(stringnow, resnow, numberslist, target, winners,verbose, i)
+                #subtract
+                resnow=ressofar-numberslist[i]
+                stringnow=stringsofar+"-"+str(numberslist[i])
+                self.validate(stringnow, resnow, numberslist, target, winners,verbose, i)
+                #divide
+                resnow=ressofar/numberslist[i]
+                stringnow="("+stringsofar+")/"+str(numberslist[i])
+                if(resnow==floor (resnow)):
+                    self.validate(stringnow, resnow, numberslist, target, winners,verbose, i)
+        
+    def validate(self,stringnow,resnow,numberslist,target,winners,verbose,i):
+        if(resnow!=target):
+            newlist=list(numberslist)
+            newlist.pop(i)
+            self.recurse(stringnow,resnow,newlist,target,winners,verbose)
+        else:
+            if(verbose):
                 print stringnow +" || Test Result "+ str(eval(stringnow) ==target)
-            #multiply with previous string
-            res1=numberslist[0]*numberslist[i]
-            stringnow="("+stringsofar+")*"+str(numberslist[i])
-            if(res1!=target):
-                newlist=list(numberslist)
-                newlist.pop(i)
-                newlist.pop(0)
-                newlist.insert(0, res1)
-                self.recurse(stringnow,newlist,target)
-            else:
-                print stringnow +" || Test Result "+  str(eval(stringnow) ==target)
-            #subtract
-            res1=numberslist[0]-numberslist[i]
-            stringnow=stringsofar+"-"+str(numberslist[i])
-            if(res1!=target):
-                newlist=list(numberslist)
-                newlist.pop(i)
-                newlist.pop(0)
-                newlist.insert(0, res1)
-                self.recurse(stringnow,newlist,target)
-            else:
-                print stringnow + " || Test Result "+ str(eval(stringnow) ==target) 
-            #divide
-            res1=numberslist[0]/numberslist[i]
-            stringnow="("+stringsofar+")/"+str(numberslist[i])
-            if(res1==floor (res1)):
-                if(res1!=target):
-                    newlist=list(numberslist)
-                    newlist.pop(i)
-                    newlist.pop(0)
-                    newlist.insert(0, res1)
-                    self.recurse(stringnow,newlist,target)
-                else:
-                    print(stringnow +" || Test Result "+  str(eval(stringnow) ==target))
+            winners.add(stringnow)
+            
+
 
 for i in range(1):          
     p=Problem()
     p.generateProblem()
-    p.numbers=[25,75,100,50,3,3]
-    p.target=996
+    #p.numbers=[50,75,100,25,3,3]
+    #p.target=996
     print p.getproblemstr()
-    print p.target
+    #print p.target
     Solver().solveProblem(p)
     #p.printproblem()
     #sleep(0.5)
